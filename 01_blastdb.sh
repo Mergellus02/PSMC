ff064dd65f46fe18ddaf650ef4996db0f40f6d9b
Vishwa Jagati <162778158+Mergellus02@users.noreply.github.com>
2024-03-11 10:46:08 +0000

    Create 01_blastdb.sh

---
+++ 01_blastdb.sh
@@ -0,0 +1,1 @@
+/home/eco-evo/softs/ncbi-blast-2.14.1+/bin/makeblastdb -in /mnt/e/psmc-tutorial/ncbi_dataset/ncbi_dataset/data/GCF_003957565.2/GCF_003957565.2_bTaeGut1.4.pri_genomic.fna -parse_seqids -dbtype nucl -out ./ZF_all_chrom.db -blastdb_version 4 > mkblastdb_zf_all.out
